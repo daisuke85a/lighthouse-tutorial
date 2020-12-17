d17c6721431bb397269b824e37c8ef38ddb998de
daisuke <dsaito85a@gmail.com>
2020-12-17 23:34:04 +0900

    add: lighthouseのtutorialを実行 https://lighthouse-php.com/tutorial/

--- src/lighthouse-tutorial/database/seeders/DatabaseSeeder.php
+++ src/lighthouse-tutorial/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Comment;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(3)->has(
+            Post::factory()->has(
+                Comment::factory()->count(2)
+            )
+        )->create();
     }
 }
